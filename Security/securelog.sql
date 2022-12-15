6f18422387dc477507d73c15e2e90cbb34b7b2de
KHIWQQ <eiq1627@gmail.com>
2022-12-16 00:14:21 +0700

    Security

---
+++ Security/securelog.sql
@@ -0,0 +1,22 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `securelog` (
+  `quater` int(11) NOT NULL,
+  `position` varchar(33) NOT NULL,
+  `fname` varchar(33) NOT NULL,
+  `sname` varchar(33) NOT NULL,
+  `onduty` int(11) NOT NULL,
+  `outduty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `securelog` (`quater`, `position`, `fname`, `sname`, `onduty`, `outduty`) VALUES
+(1, '1', 'AAA', 'AAA', 1, 0),
+(1, '2', 'BBB', 'BBB', 1, 0),
+(1, '3', 'CCC', 'CCC', 1, 0),
+(1, '4', 'DDD', 'DDD', 1, 0),
+(1, '5', 'EEE', 'EEE', 1, 0),
+(1, '6', 'FFF', 'FFF', 1, 0),
+(1, '7', 'ZZZ', 'ZZZ', 1, 0);
+COMMIT;
---
+++ Security/securestatus.sql
@@ -0,0 +1,22 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `securestatus` (
+  `quater` int(11) NOT NULL,
+  `fname` varchar(33) NOT NULL,
+  `sname` varchar(33) NOT NULL,
+  `company` int(11) NOT NULL,
+  `platoon` int(11) NOT NULL,
+  `position` varchar(22) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `securestatus` (`quater`, `fname`, `sname`, `company`, `platoon`, `position`) VALUES
+(1, 'AAA', 'AAA', 1, 1, '1'),
+(1, 'BBB', 'BBB', 1, 1, '2'),
+(1, 'CCC', 'CCC', 1, 1, '3'),
+(1, 'DDD', 'DDD', 2, 1, '4'),
+(1, 'EEE', 'EEE', 2, 1, '5'),
+(1, 'FFF', 'FFF', 3, 1, '6'),
+(1, 'ZZZ', 'ZZZ', 3, 1, '7');
+COMMIT;
